d7a8606a85e9e3b2e6ff1c657d5130a3d505d90c
Lhoussaine <bouaik.lhou@gmail.com>
2020-10-13 06:59:42 +0100

    Added navbar and created pages components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Navs from './components/Navs';
+import Home from './pages/Home';
+import Starred from './pages/Starred';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/">
-        This is home page
-      </Route>
+    <>
+      <Navs />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
 
-      <Route exact path="/starred">
-        This is starred page
-      </Route>
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
 
-      <Route>404 not found</Route>
-    </Switch>
+        <Route>
+          <div>404 not found</div>
+        </Route>
+      </Switch>
+    </>
   );
 }
 
---
+++ src/components/Navs.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const LINKS = [
+  {
+    to: '/',
+    text: 'Home',
+  },
+  {
+    to: '/starred',
+    text: 'Starred',
+  },
+];
+
+function Navs() {
+  return (
+    <div>
+      <ul>
+        {LINKS.map(link => (
+          <li key={link.to}>
+            <Link to={link.to}>{link.text}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Navs;
